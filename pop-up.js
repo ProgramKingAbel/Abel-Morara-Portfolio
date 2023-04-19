5b2ef43667b5d03c5d4619ba8d2bcb43cc843cf9
ProgramKingAbel <abelmorara254@gmail.com>
2023-04-19 13:31:15 +0100

    added pop-up styling and js

---
+++ pop-up.js
@@ -0,0 +1,54 @@
+const projectsArea = document.querySelector('.projects');
+const modalPopUp = document.querySelector('#modal');
+
+const projects = [
+    {
+        id: 1,
+        name: 'Tonic',
+        description: 'A daily selection of privately personalized reads; no accounts or sign-ups required',
+        featureImg: '/images/Nature.svg',
+        technology: ['html', 'css', 'javascript'],
+        platform: 'CANOPY',
+        stack: 'Back End Dev',
+        year: 2015,
+        livelink: 'https://programkingabel.github.io/',
+        linkSource: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+        
+    },
+    {
+        id: 2,
+        name: 'Multi-Post Stories',
+        description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+        featureImg: '/images/grd4.svg',
+        technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
+        platform: 'FACEBOOK',
+        stack: 'Full Stack Dev',
+        year: 2015,
+        liveLink: 'https://programkingabel.github.io/',
+        linkSource: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+    },
+    {
+        id: 3,
+        name: 'Facebook 360',
+        description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+        featureImg: '/images/Tonic.svg',
+        technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
+        platform: 'FACEBOOK',
+        stack: 'Full Stack Dev',
+        year: 2015,
+        liveLink: 'https://programkingabel.github.io/',
+        linkSource: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+    },
+    {
+        id: 4,
+        name: 'Uber Navigation',
+        description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+        featureImg: '/images/grd2.svg',
+        technology: ['html', 'Ruby on Rails', 'css', 'javascript'],
+        platform: 'Uber',
+        stack: 'Lead Developer',
+        year: 2018,
+        liveLink: 'https://programkingabel.github.io/',
+        linkSource: 'https://github.com/ProgramKingAbel/Abel-Morara-Portfolio',
+    },
+];
