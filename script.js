58fee6a49f68ee73a42be1394de121896a1be299
ProgramKingAbel <abelmorara254@gmail.com>
2023-04-20 18:17:01 +0100

    Installed and configured eslint

--- script.js
+++ script.js
@@ -1,64 +1,60 @@
 // Form validation goes here
 
-//show message following the type of input
 function showMessage(input, message, type) {
-    const msg = input.parentNode.querySelector("small");
-    msg.innerHTML = message;
-  
-    //update input class
-    input.className = type ? "success" : "error";
-    return type;
-  }
-  function showError(input, message) {
-    return showMessage(input, message, false);
-  }
-  function showSuccess(input) {
-    return showMessage(input, "", true);
-  }
-  function hasValue(input, message) {
-    if (input.value.trim() === "") {
-      return showError(input, message);
-  
-    }
-    return showSuccess(input);
-  }
-  
-  function validateEmail(input, requiredMsg, invalidMsg) {
-    //check if value is not empty
-    
-      if (!hasValue(input, requiredMsg)) {
-        return false;
-    }
-    //validate email RegEx 
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
-  
-    
-    const email = input.value.trim();
-  
-    if (!emailRegex.test(email)) {
-      return showError(input, invalidMsg);
-  
-    }
-    return true;  
-  }
+  const msg = input.parentNode.querySelector('small');
+  msg.innerHTML = message;
 
-  const form = document.getElementById("register");
-  const NAME_REQ = "Please Enter your name";
-  const MSG_REQ = "Field Cannot be empty";
-  const EMAIL_REQ = "Please Enter Your email";
-  const EMAIL_INVALID = "Please Enter a correct email address in Lowercase";
-  const btn = document.getElementById('submit-btn');
-  
-   btn.addEventListener("click", function (event) {
-    //reject submission of form
-    event.preventDefault();
-  
-    //validate form
-    let nameValid = hasValue(form.elements["name"], NAME_REQ);
-    let emailValid = validateEmail(form.elements["email"], EMAIL_REQ, EMAIL_INVALID);
-    let msgValid = hasValue(form.elements["message"], MSG_REQ);
-    //if valid submit form
-    if (nameValid && emailValid && msgValid) {
-        form.submit();
-    }
+  // update input class
+  input.className = type ? 'success' : 'error';
+  return type;
+}
+function showError(input, message) {
+  return showMessage(input, message, false);
+}
+function showSuccess(input) {
+  return showMessage(input, '', true);
+}
+function hasValue(input, message) {
+  if (input.value.trim() === '') {
+    return showError(input, message);
+  }
+  return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+  // check if value is not empty
+
+  if (!hasValue(input, requiredMsg)) {
+    return false;
+  }
+  // validate email RegEx
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-z\-0-9]+\.)+[a-z]{2,}))$/;
+
+  const email = input.value.trim();
+
+  if (!emailRegex.test(email)) {
+    return showError(input, invalidMsg);
+  }
+  return true;
+}
+
+const form = document.getElementById('register');
+const NAME_REQ = 'Please Enter your name';
+const MSG_REQ = 'Field Cannot be empty';
+const EMAIL_REQ = 'Please Enter Your email';
+const EMAIL_INVALID = 'Please Enter a correct email address in Lowercase';
+const btn = document.getElementById('submit-btn');
+
+btn.addEventListener('click', (event) => {
+  // reject submission of form
+  event.preventDefault();
+
+  // validate form
+  const nameValid = hasValue(form.elements.name, NAME_REQ);
+  const emailValid = validateEmail(form.elements.email, EMAIL_REQ, EMAIL_INVALID);
+  const msgValid = hasValue(form.elements.message, MSG_REQ);
+  // if valid submit form
+  if (nameValid && emailValid && msgValid) {
+    form.submit();
+  }
 });
